d50f071a4c36647bd5dd88e8b9aa9f46d7ba94fe
Ivan Jerzabek <jerzabek.ivan@gmail.com>
2023-09-10 11:47:45 +0200

    Migrate to bun

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>Testing</p>
+        <p>This is Pinehaus</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"

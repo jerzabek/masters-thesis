1ff0aaf42424b0c3277c877f5e4b125c2d1ee45e
Ivan Jerzabek <jerzabek.ivan@gmail.com>
2023-09-27 22:28:53 +0200

    Revert Bun

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>This is Pinehaus</p>
+        <p>Testing</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"

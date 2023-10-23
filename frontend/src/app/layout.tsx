65b381648f4dc2700f4939ab7a3bf9d1de95e009
Ivan Jerzabek <jerzabek.ivan@gmail.com>
2023-10-23 21:45:55 +0200

    Basic sliding banner on homepage

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
 
-import dynamic from 'next/dynamic'
 import StyledComponentsRegistry from './modules/ui/StyledComponentRegistry'
-
-dynamic(() => require('bootstrap/dist/js/bootstrap.min.js'), { ssr: false })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
--- frontend/src/app/modules/LandingBanner/LandingBanner.tsx
+++ frontend/src/app/modules/LandingBanner/LandingBanner.tsx
@@ -1,32 +1,55 @@
+import 'bootstrap/dist/js/bootstrap.min.js'
 import * as S from './styles'
-
-import Foodie from '../../../../public/images/banners/foodie.jpg'
-import styled from 'styled-components'
+import { Carousel, CarouselItem } from 'react-bootstrap'
 
 export default function LandingBanner() {
+  const stores = [
+    {
+      name: 'VinylVerse',
+      imageUrl:
+        'https://images.pexels.com/photos/6865916/pexels-photo-6865916.jpeg?auto=compress&cs=tinysrgb&w=2560&h=1440&dpr=1',
+      description: 'A treasure trove of vintage and new vinyl, where music comes alive.',
+    },
+    {
+      name: 'Nourish',
+      imageUrl:
+        'https://images.pexels.com/photos/1397514/pexels-photo-1397514.jpeg?auto=compress&cs=tinysrgb&w=2560&h=1440&dpr=1',
+      description: 'Artisanal oasis offering global flavors, fresh produce, and culinary inspiration.',
+    },
+    {
+      name: 'Bauhaus',
+      imageUrl:
+        'https://images.pexels.com/photos/4483774/pexels-photo-4483774.jpeg?auto=compress&cs=tinysrgb&w=2560&h=1440&dpr=1',
+      description:
+        ' Innovative art school emphasizing functional design, merging art, craft, and technology harmoniously.',
+    },
+  ]
+
   return (
-    <S.Banner>
-      <S.BannerImage src={Foodie} alt="Banner image" />
+    <Carousel>
+      {stores.map(({ name, imageUrl, description }) => (
+        <CarouselItem key={name}>
+          <S.Banner>
+            <S.BannerImage src={imageUrl} width={2000} height={600} alt="Banner image" />
 
-      <S.BannerContent>
-        <div>
-          <S.BannerTitle className="px-3">
-            <strong>Foodie</strong>
-          </S.BannerTitle>
-          <span>
-            <em>Featured seller</em>
-          </span>
-        </div>
+            <S.BannerContent>
+              <div>
+                <S.BannerTitle className="px-3">
+                  <strong>{name}</strong>
+                </S.BannerTitle>
+                <span>
+                  <em>Featured seller</em>
+                </span>
+              </div>
 
-        <div className="text-center py-3 px-5" style={{ maxWidth: '400px', backgroundColor: '#101010cc' }}>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates nam quam officiis dicta saepe
-            voluptatibus iste possimus alias omnis cupiditate.
-          </p>
-          <p>Shop at our store! -Foodie</p>
-          <button className="btn btn-success btn-lg rounded-1 mt-2">Shop now!</button>
-        </div>
-      </S.BannerContent>
-    </S.Banner>
+              <div className="text-center py-3 px-5" style={{ maxWidth: '400px', backgroundColor: '#101010cc' }}>
+                <p>{description}</p>
+                <button className="btn btn-outline-light btn-lg rounded-1 mt-2">Shop now!</button>
+              </div>
+            </S.BannerContent>
+          </S.Banner>
+        </CarouselItem>
+      ))}
+    </Carousel>
   )
 }

d7c344bde11e0b5d87aff629c114cda478331935
Ivan Jerzabek <jerzabek.ivan@gmail.com>
2024-02-28 22:07:31 +0100

    Add chakra

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import { StyledComponentsRegistry } from 'shared/ui/StyledComponentRegistry'
+import 'app/(styles)/globals.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <ChakraProvider>
+          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        </ChakraProvider>
       </body>
     </html>
   )
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Flex, Text } from '@chakra-ui/react'
+
 export default function Home() {
   return (
     <main>
-      <div>
-        <h1>Hello world!</h1>
-      </div>
+      <Flex h="100%" justify="center" align="center">
+        <Text as="h1" fontSize="2.5em">
+          Hello world
+        </Text>
+      </Flex>
     </main>
   )
 }

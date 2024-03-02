0c166ee0c2b1b20335da49971b96ef12f617431a
Ivan Jerzabek <jerzabek.ivan@gmail.com>
2024-03-03 00:52:18 +0100

    Navigation bar & Link

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,13 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { StyledComponentsRegistry } from 'utils/ui/StyledComponentRegistry'
 import 'styles/global/globals.css'
+import Navigation from 'components/Navigation'
+import Footer from 'components/Footer'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         <ChakraProvider>
-          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+          <StyledComponentsRegistry>
+            <Navigation />
+            {children}
+            <Footer />
+          </StyledComponentsRegistry>
         </ChakraProvider>
       </body>
     </html>

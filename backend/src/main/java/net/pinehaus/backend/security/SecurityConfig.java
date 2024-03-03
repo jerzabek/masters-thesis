6ab5bd22de3acbfbf499aa96822a985461bde6e9
Ivan Jerzabek <jerzabek.ivan@gmail.com>
2024-03-03 20:16:49 +0100

    Temporarily permit all connections

--- backend/src/main/java/net/pinehaus/backend/security/SecurityConfig.java
+++ backend/src/main/java/net/pinehaus/backend/security/SecurityConfig.java
@@ -2,26 +2,32 @@ package net.pinehaus.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+//                        .requestMatchers("/", "/error", "/webjars/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .exceptionHandling(e -> e
+//                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+//                )
+//                .httpBasic(Customizer.withDefaults())
+//                .oauth2Login(Customizer.withDefaults());
         http
-                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
-                        .requestMatchers("/", "/error", "/webjars/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .exceptionHandling(e -> e
-                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-                )
-                .oauth2Login(Customizer.withDefaults());
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("*").permitAll()
+                        .anyRequest().authenticated());
 
         return http.build();
     }

0e269ba843a4ffd059fc9e4860ffa031c23c6642
Ivan Jerzabek <jerzabek.ivan@gmail.com>
2024-03-08 13:00:47 +0100

    Add OpenAPI api information

--- backend/src/main/java/net/pinehaus/backend/user/controller/UserController.java
+++ backend/src/main/java/net/pinehaus/backend/user/controller/UserController.java
@@ -1,5 +1,11 @@
 package net.pinehaus.backend.user.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import net.pinehaus.backend.user.model.User;
 import net.pinehaus.backend.user.service.UserService;
@@ -14,11 +20,16 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Users", description = "User API")
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping("/user/{id}")
+    @Operation(summary = "Get a user by ID.", description = "Fetch user by ID, if it exists.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema())})})
     public User getUser(@PathVariable UUID id) {
         Optional<User> user = userService.getUserById(id);
 

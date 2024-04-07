d0e19feae39a70918ec301a7d37fe65428c835ca
Ivan Jerzabek <jerzabek.ivan@gmail.com>
2024-04-07 21:57:01 +0200

    [Frontend:Nav] Close collapsible on click

--- frontend/src/components/Navigation/components/MobileNavigation/MobileNavigation.tsx
+++ frontend/src/components/Navigation/components/MobileNavigation/MobileNavigation.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import NextLink from 'next/link'
 import { Box, Button, Flex, Text, useDisclosure, Collapse } from '@chakra-ui/react'
 import Link from 'components/Link'
@@ -6,12 +6,28 @@ import { useUser } from 'hooks/authentication'
 import { Menu, UserAvatar } from '@carbon/icons-react'
 
 const MobileDropdownNav = () => {
-  const { isOpen, onToggle } = useDisclosure()
+  const { isOpen, onToggle, onClose } = useDisclosure()
+
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
+        onClose()
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [containerRef, onToggle])
 
   const { user } = useUser()
 
   return (
-    <Flex direction="column">
+    <Flex direction="column" ref={containerRef}>
       <Flex align="center" py={2} px={4}>
         <NextLink href="/">
           <Flex align="center">
@@ -41,13 +57,13 @@ const MobileDropdownNav = () => {
       <Collapse in={isOpen} animateOpacity>
         <Box p="40px" shadow="md">
           <Flex direction="column">
-            <Link href={`/`}>
+            <Link href={`/`} onClick={onClose}>
               <Text fontSize={20}>Home</Text>
             </Link>
-            <Link href={`/products`}>
+            <Link href={`/products`} onClick={onClose}>
               <Text fontSize={20}>Products</Text>
             </Link>
-            <Link href={`/about-us`}>
+            <Link href={`/about-us`} onClick={onClose}>
               <Text fontSize={20}>About</Text>
             </Link>
           </Flex>

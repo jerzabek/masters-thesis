2853be33ed87b91e3aa30b64184233b3b9e97463
Ivan Jerzabek <jerzabek.ivan@gmail.com>
2024-05-19 23:38:26 +0200

    [Frontend] Revalidate fetch tag

--- frontend/src/api/Product/repository.ts
+++ frontend/src/api/Product/repository.ts
@@ -1,12 +1,14 @@
 import { Product } from 'model/Product'
 import { deleteJson, getJson, postJson, putJson } from 'utils/api'
 
+import { PRODUCT_FETCH_TAG } from './const'
 import * as I from './interface'
 import * as R from './routes'
 
 export const getProducts = (query: I.ProductListFilters) => getJson<I.ProductListResponse>(R.getProductList(query))
 
-export const getProduct = (id: number) => getJson<I.GetProductResponse>(R.getProduct(id), {})
+export const getProduct = (id: number) =>
+  getJson<I.GetProductResponse>(R.getProduct(id), { next: { tags: [PRODUCT_FETCH_TAG] } })
 
 export const updateProduct = (id: number, payload: I.UpdateProductPayload) =>
   putJson<Product>(R.updateProduct(id), payload)

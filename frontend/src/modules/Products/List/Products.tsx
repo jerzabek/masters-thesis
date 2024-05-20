3433013c1b3f33ad7afdd709c7f838815835e3e5
Ivan Jerzabek <jerzabek.ivan@gmail.com>
2024-05-20 11:27:10 +0200

    [Frontend] Product list initial filter query

--- frontend/src/modules/Products/List/Products.tsx
+++ frontend/src/modules/Products/List/Products.tsx
@@ -1,12 +1,13 @@
 import { getProducts } from 'api/Product/repository'
 
 import { ProductList } from './components'
+import { INITIAL_PRODUCTS_STATE } from './const'
 
 export default async function Products() {
   let products
 
   try {
-    products = await getProducts({})
+    products = await getProducts({ size: INITIAL_PRODUCTS_STATE.size, sort: INITIAL_PRODUCTS_STATE.sort })
   } catch (e) {
     console.error(e)
     return null
